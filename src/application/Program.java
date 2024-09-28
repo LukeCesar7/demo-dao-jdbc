b49fffc221aef3091983bd1000fb09a4826551ce
Luquian Cesar <luquian.santos@outlook.com>
2024-09-28 15:46:10 -0300

    feat: Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         //Criação de objeto, o = obj
         Department o = new Department(1, "Books");
-        System.out.println(o);
+        Seller seller = new Seller(21, "John Gree", "johngree@gamil.com", new Date(), 3000.0, o);
+        System.out.println(seller);
     }
 }
