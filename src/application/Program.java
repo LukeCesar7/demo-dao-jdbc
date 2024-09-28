3e730d7f990b413151bf61ad7dbda6fdf5744052
Luquian Cesar <luquian.santos@outlook.com>
2024-09-28 15:06:40 -0300

    feat: Departament entity class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        //Criação de objeto, o = obj
+        Department o = new Department(1, "Books");
+        System.out.println(o);
+    }
+}
