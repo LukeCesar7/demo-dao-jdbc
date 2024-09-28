087746ed98a2166e6c5f209bd8acbb9ba80749ef
Luquian Cesar <luquian.santos@outlook.com>
2024-09-28 15:57:46 -0300

    feat: DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller o);
+    void update(Seller o);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
+
+
+
