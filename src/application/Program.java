287d8097230e7c821910fa6dcff411a645612f00
Luquian Cesar <luquian.santos@outlook.com>
2024-09-28 19:46:28 -0300

    feat: SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,7 +12,12 @@ public class Program {
 
         //Criação de objeto, o = obj
         Department o = new Department(1, "Books");
+
         Seller seller = new Seller(21, "John Gree", "johngree@gamil.com", new Date(), 3000.0, o);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
+
     }
 }
